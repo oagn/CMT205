140363b88f529be60e845f55e41d5cf3b724f423
oagn <agnethe.s.olsen@gmail.com>
2018-02-15 12:27:26 +0000

    Skeleton code for exercise 1, week 3

---
+++ Week3/ArraySumming.java
@@ -0,0 +1,9 @@
+/* Week 3, exercise 1:
+- Create an array cotaining integers 3,5,7,9,10.
+- Use a for loop to calculate and print their sum. */
+
+public class ArraySumming{
+  public static void main (String[] args){
+
+  }
+}
