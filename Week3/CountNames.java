61da25e20d6da2fd579422612331b0047f09a40f
oagn <agnethe.s.olsen@gmail.com>
2018-02-15 12:52:43 +0000

    Task 2, a. week3, counting Jones appearences

--- Week3/CountNames.java
+++ Week3/CountNames.java
@@ -1,10 +1,19 @@
+/* Week 3, exercise 2:
+- a) Count the number of times an actor's name is Jones.
+- b) Modify to count all versions of Jones, also upper case. */
+
 public class CountNames {
 	public static void main( String args[] ) {
-        
+
         String[] names = {"Tommy Lee Jones", "William Hurt", "Nicole Kidman",
-            "Catherine Zeta-Jones", "JAMES EARL JONES", "Tom Hardy", 
+            "Catherine Zeta-Jones", "JAMES EARL JONES", "Tom Hardy",
             "Judi Dench", "Sean Bean", "Carey Mulligan", "Toby Jones" };
-        
+        int count = 0;
+				for (String name :names){
+					if (name.contains("Jones")){
+						count++;
+					}
+				}
+				System.out.println(count);
 	}
 }
-
