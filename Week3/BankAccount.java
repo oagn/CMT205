a8aaa1067d6883201a5182da798abf93f55ffa8a
oagn <agnethe.s.olsen@gmail.com>
2018-02-18 14:47:36 +0000

    Week 3, task 5, Bank account class with testing code

--- Week3/BankAccount.java
+++ Week3/BankAccount.java
@@ -1,5 +1,60 @@
+/* Week 3, exercise 5:
+- a) Add a constructor that takes two arguments, specifying name and account number
+- b) Appropriate accessor and mutator methods
+- c) Add methods to deposit to, check balance and withdraw from account */
+
 public class BankAccount {
-    private ??? holderName;
-    private ??? balance;
-    private ??? number;
+    private String holderName;
+    private double balance;
+    private String number;
+
+    /// Constructor for BankAccount
+    public BankAccount(String holderName, String number){
+        this.holderName = holderName;
+        this.number = number;
+        this.balance = 0.0;
+    }
+
+    /// Accessor for name of the accoun holder
+    public String getName(){
+        return holderName;
+    }
+
+    /// Mutator for name of the account holder
+    public void setName(String newName){
+        holderName = newName;
+    }
+
+    /// Accessor for bank abbount number
+    public String getNumber(){
+        return number;
+    }
+
+    /// Mutator for bank account number
+    public void setNumber(String newNumber){
+        number = newNumber;
+    }
+
+    /// Method to deposit to the bank account
+    public void deposit(double addMoney){
+        balance += addMoney;
+    }
+
+    /// Method to chack balance of bank account
+    public double checkBalance(){
+        return balance;
+    }
+
+    /// Method to withdraw from bank account
+    public void withdraw(double withdrawal){
+        if(balance - withdrawal >= 0){
+            balance -= withdrawal;
+        }
+        else System.out.printf("You have insufficient funds (£%.2f) for a withdrawal of £%.2f \n",balance,withdrawal);
+    }
+
+    public String toString(){
+        String s = holderName + "\t(" + number + ":\t£" + balance + ") ";
+        return s;
+    }
 }
---
+++ Week3/BankTest.java
@@ -0,0 +1,52 @@
+/* Week 3, exercise 5:
+- Test the BankAccount class */
+public class BankTest{
+
+    public static void main(String[] args){
+        // Create a new account
+        System.out.println("\nSTEP 1:\n Create a new bank account.");
+        BankAccount a = new BankAccount("M J W Morgan", "0012067");
+        System.out.println(a);
+
+        // Get and set names
+        System.out.println("\nSTEP 2:\n Get account holder name.");
+        String name = a.getName();
+        System.out.println("The name is " + name);
+        System.out.println(a);
+
+        System.out.println("\nSTEP 3:\n Set account holder name.");
+        a.setName("Mary Berry");
+        System.out.println(a);
+
+        // Get and set account number
+        System.out.println("\nSTEP 4:\n Get account number.");
+        String number = a.getNumber();
+        System.out.println("The account number is " + number);
+        System.out.println(a);
+
+        System.out.println("\nSTEP 5:\n Set account number.");
+        a.setNumber("0012044");
+        System.out.println(a);
+
+        // Withdraw money with no funds
+        System.out.println("\nSTEP 6:\n Attempt to withdraw money with insufficient funds.");
+        a.withdraw(22.0);
+        System.out.println(a);
+
+        // Deposit to account
+        System.out.println("\nSTEP 7:\n Deposit to bank account.");
+        a.deposit(50.0);
+        System.out.println(a);
+
+        // Check balance
+        System.out.println("\nSTEP 8:\n Check account balance.");
+        double balance = a.checkBalance();
+        System.out.println("The balance is " + balance);
+        System.out.println(a);
+
+        // Withdraw with sufficient funds
+        System.out.println("\nSTEP 9:\n Withdraw from account.");
+        a.withdraw(22.0);
+        System.out.println(a);
+    }
+}
