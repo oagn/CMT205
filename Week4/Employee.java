7f98f38b745c8b75205bf75f8656d75c7c7ee10c
oagn <agnethe.s.olsen@gmail.com>
2018-02-22 12:11:16 +0000

    Week 4, task 1, accessors and mutators added to Employee class

--- Week4/Employee.java
+++ Week4/Employee.java
@@ -36,4 +36,40 @@ public class Employee{
         this.position = position;
     }
 
+    // accessor and mutator for name
+    public String getName( ){
+        return name;
+    }
+
+    public void setName( String name ){
+        this.name = name;
+    }
+
+    // accessor and mutator for idNumber
+    public int getIdNumber( ){
+        return idNumber;
+    }
+
+    public void setIdNumber( int idNumber ){
+        this.idNumber = idNumber;
+    }
+
+    // accessor and mutator for department
+    public String getDepartment( ){
+        return department;
+    }
+
+    public void setDepartment( String department ){
+        this.department = department;
+    }
+
+    // accessor and mutator for position
+    public String getPosition( ){
+        return position;
+    }
+
+    public void setPosition( String position ){
+        this.position = position;
+    }
+
 }
