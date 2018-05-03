7efb2faeac793cf66b297c9b42bd99d3c1073d5f
oagn <agnethe.s.olsen@gmail.com>
2018-05-03 14:40:43 +0100

    Code for testing implemented

--- Week11/RandomArray/src/RandomArrayTester.java
+++ Week11/RandomArray/src/RandomArrayTester.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /* Task 3
  * Create an array list with 10 random numbers, 
  * the use the command pattern to:
@@ -8,6 +10,31 @@
 
 public class RandomArrayTester{
 	public  static void main(String[] args) {
+		// create a collection
+		RandomArray  collection = new RandomArray();
+		// add elements to the collection
+		Random rand = new Random();
+		for (int j = 0; j<10; j++)
+        {
+            double pick = rand.nextDouble();
+            collection.add(pick);
+        }
 		
+		// print out the elements of the collection
+		collection.map(new Command() {
+		    public void execute(Object data) {
+		      System.out.println(data);
+		   }
+		});
+		collection.map(new Command() {
+		    public void execute(Object data) {
+		      ;
+		   }
+		});
+		collection.map(new Command() {
+		    public void execute(Object data) {
+		      System.out.println(data);
+		   }
+		});
 	}
 }
