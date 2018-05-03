70cfa794fe08ce5321b33d6e1eb90c3afdddd6b3
oagn <agnethe.s.olsen@gmail.com>
2018-05-03 12:42:24 +0100

    Initial commit Week11, task2, iteration over virtual collection of prime numbers

---
+++ Week11/SetOfPrimes/src/SetOfPrimes.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+
+/* Task 2:
+ * Create a virtual collection of primes. 
+ * Use lazy evaluation to only compute the numbers when they are needed.
+*/
+
+public class SetOfPrimes {
+	// Constructor
+	public SetOfPrimes() {
+		
+	}
+	
+	// Return i-th prime
+	public Integer get (Integer i) {
+		Integer prime = null;
+		return prime;
+	}
+	
+	// returns an iterator over the primes. The n-th call to
+	// the iterator's next() method returns and Integer value
+	// containing the n-th largest prime. 
+	public Iterator<Integer> iterator(){
+		Iterator<Integer> iterator = null;
+		return iterator;
+	}
+}
